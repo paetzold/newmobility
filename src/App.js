603d68b7d7aaea9a5834e198776b9311e2e4ef7b
Kay P <kay.paetzold@gmail.com>
2021-02-11 17:55:19 +0100

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Container } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Button color="primary">Hello World</Button>
+      <Button color="danger">Hello World</Button>
+    </Container>
   );
 }
 
 export default App;
+
+
