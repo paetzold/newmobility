064d56800fa4e0251378cc72e223fba877253032
Kay P <kay.paetzold@gmail.com>
2021-02-11 22:40:11 +0100

    Test

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <Button color="danger">Hello World 2</Button>
       <Button color="danger">Hello World 3</Button>
       <Button color="danger">Hello World !!!!</Button>
+      <Button color="danger">Juhu</Button>
     </Container>
   );
 }
