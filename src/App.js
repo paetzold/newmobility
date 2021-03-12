e75c1cfaa2cdc96d1ba98a6b92d3f15c1d97f775
Kay P <kay.paetzold@gmail.com>
2021-03-12 09:13:44 +0100

    before cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { Button, Container } from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { Button, Container, Paper, Card,CardContent, Typography} from '@material-ui/core';
+import { QRCode } from 'react-qrcode-logo';
+
+import Download from './Download';
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 function App() {
   return (
-    <Container>
-      <Button color="primary">Hello World</Button>
-    </Container>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Download />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
 export default App;
 
 
